eb243454c7a078617fce694f020d468dcaa7627d
Sophie Smithburg <sophie@heartpunk.com>
2023-10-01 00:10:08 -0700

    testssss

--- internal/pkg/instrumentors/bpf/google.golang.org/grpc/bpf/probe.bpf.c
+++ internal/pkg/instrumentors/bpf/google.golang.org/grpc/bpf/probe.bpf.c
@@ -70,6 +70,9 @@ struct
 struct
 {
     __uint(type, BPF_MAP_TYPE_PERF_EVENT_ARRAY);
+        __uint(key_size, sizeof(u32));
+        __uint(value_size, sizeof(u32));
+        __uint(max_entries, 20);
 } events SEC(".maps");
 
 // Injected in init
